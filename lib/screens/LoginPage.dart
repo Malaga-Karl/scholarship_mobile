1f13fbc5dc9e18d8f205c7779cb0a56dfd803c1f
Malaga-Karl <karlmalaga06@gmail.com>
2024-12-02 00:25:30 +0800

    login shite

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -4,13 +4,73 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Add your login logic here
-            Navigator.pushNamed(context, '/home');
-          },
-          child: Text('Login with Microsoft'),
+      appBar: AppBar(
+        title: Text('PLM Scholarship System'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        // padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  opacity: 0.6,
+                  image: AssetImage('assets/plmLogo.jpg'),
+                  fit: BoxFit.cover
+                  ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 200),
+                child: Column(
+                  children: [
+                    Center(
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>( RoundedRectangleBorder( borderRadius: BorderRadius.circular(4), )),
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                        ),
+                        
+                        onPressed: () {
+                          // Add your login logic here
+                        },
+                        child: Text('Login with Microsoft'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  Text(
+                    'PLM Scholarship System',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'The PLM Scholarship System is a web-based platform of the Resource Generation Office for Pamantasan ng Lungsod ng Maynila that streamlines scholarship applications. Students can apply online, track their status, and stay updated on opportunities, promoting efficiency and supporting academic growth through accessible technology.',
+                  ),
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Center(
+                      child: Text(
+                        'Copyright © 2024 Pamantasan ng Lungsod ng Maynila. All Rights Reserved.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  ),
+                    ],
+                ),
+            )
+          ],
         ),
       ),
     );
