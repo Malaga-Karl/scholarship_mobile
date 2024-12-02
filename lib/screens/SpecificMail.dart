f23efd88b08bc3f2ae186304a5a7165599dd5547
Malaga-Karl <karlmalaga06@gmail.com>
2024-12-03 04:12:36 +0800

    pinagandang specific mail

--- lib/screens/SpecificMail.dart
+++ lib/screens/SpecificMail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:scholarhip_mobile/models/Mail.dart'; // Import Mail model
-import 'package:scholarhip_mobile/data/mailData.dart'; // Import Mail data
+import 'package:scholarhip_mobile/data/mailData.dart';
+
+import '../colors.dart'; // Import Mail data
 
 class SpecificMail extends StatelessWidget {
   final String id;
@@ -12,19 +14,35 @@ class SpecificMail extends StatelessWidget {
     final mail = MailData().mailList.firstWhere((mail) => mail.id == id);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(mail.subject),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              decoration: BoxDecoration(
+                color: CustomColors().blue
+              ),
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(mail.subject, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.white), textAlign: TextAlign.center,),
+              )
+            ),
+            SizedBox(height: 32),
             Text("From: ${mail.sender.name} <${mail.sender.email}>", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             Text("To: ${mail.receiver.name} <${mail.receiver.email}>", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
             Text("Sent: ${mail.sentTime}", style: TextStyle(fontSize: 16)),
             SizedBox(height: 16),
             Text(mail.body, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 16,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(onPressed: (){}, child: Text("Delete")),
+                TextButton(onPressed: (){}, child: Text("Reply"))
+              ],
+            )
           ],
         ),
       ),
